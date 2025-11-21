e6ba4f99edbf3805ffd031d7b3fd770820ae08bb
Ahhasha <elenacheoai@gmail.com>
2025-11-22 01:39:02 +0300

    feat: подключение к БД, стартовая миграция

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"pr-reviewer-service-AVITO2025/internal/repository"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	ctx := context.Background()
+
+	databaseURL := "postgresql://postgres:pr_service_password@localhost:5432/pr_service?sslmode=disable"
+
+	pool, err := repository.NewPool(ctx, databaseURL)
+	if err != nil {
+		log.Fatalf("Ошибка подключения к БД: %v", err)
+	}
+	defer pool.Close()
+
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("RABOTAET"))
+	})
+
+	log.Println("Server start on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
