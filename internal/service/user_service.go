c202d1079ac8a6ee526246345d48e0017042631d
Ahhasha <elenacheoai@gmail.com>
2025-11-22 21:38:08 +0300

    feat: создание UserService для бизнес-логики user

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,22 +2,29 @@ package service
 
 import (
 	"context"
+	"log/slog"
 	"pr-reviewer-service-AVITO2025/internal/api"
 	"pr-reviewer-service-AVITO2025/internal/storage/postgres"
 )
 
 type UserService struct {
 	userRepo *postgres.UserRepo
+	lgr      *slog.Logger
 }
 
-func NewUserService(userRepo *postgres.UserRepo) *UserService {
-	return &UserService{userRepo: userRepo}
+func NewUserService(userRepo *postgres.UserRepo, lgr *slog.Logger) *UserService {
+	return &UserService{
+		userRepo: userRepo,
+		lgr:      lgr,
+	}
 }
 
 func (s *UserService) SetUserActive(ctx context.Context, userID string, isActive bool) (*api.User, error) {
 	user, err := s.userRepo.SetUserActive(ctx, userID, isActive)
 	if err != nil {
+		s.lgr.Error("failed to set user active", "method", "SetUserActive", "user_id", userID, "is_active", isActive, "error", err)
 		return nil, err
 	}
+	s.lgr.Info("user active update", "user_id", userID, "is_active", isActive, "username", user.Username)
 	return user, nil
 }
