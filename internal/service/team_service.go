cc22871c50ca2e515b0c1736758158b150d280a6
Ahhasha <elenacheoai@gmail.com>
2025-11-22 21:46:28 +0300

    feat: добавил логирование в сервисы

--- internal/service/team_service.go
+++ internal/service/team_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"log/slog"
 
 	"pr-reviewer-service-AVITO2025/internal/api"
 	"pr-reviewer-service-AVITO2025/internal/storage/postgres"
@@ -9,21 +10,33 @@ import (
 
 type TeamService struct {
 	teamRepo *postgres.TeamRepo
+	lgr      *slog.Logger
 }
 
-func NewTeamService(teamRepo *postgres.TeamRepo) *TeamService {
-	return &TeamService{teamRepo: teamRepo}
+func NewTeamService(teamRepo *postgres.TeamRepo, lgr *slog.Logger) *TeamService {
+	return &TeamService{
+		teamRepo: teamRepo,
+		lgr:      lgr,
+	}
 }
 
 func (s *TeamService) CreateTeam(ctx context.Context, team *api.Team) (*api.Team, error) {
 
 	createdTeam, err := s.teamRepo.Create(ctx, team)
 	if err != nil {
+		s.lgr.Error("failed to create team", "method", "CreateTeam", "team_name", team.TeamName, "error", err)
 		return nil, err
 	}
+	s.lgr.Info("team created", "team_name", team.TeamName, "members_count", len(team.Members))
 	return createdTeam, nil
 }
 
 func (s *TeamService) GetTeam(ctx context.Context, teamName string) (*api.Team, error) {
-	return s.teamRepo.GetByName(ctx, teamName)
+	team, err := s.teamRepo.GetByName(ctx, teamName)
+
+	if err != nil {
+		s.lgr.Error("failed to get team", "GetTeam", "team_name", teamName, "error", err)
+		return nil, err
+	}
+	return team, nil
 }
