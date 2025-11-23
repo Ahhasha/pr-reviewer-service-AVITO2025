b8d17aaa80031cba44ca987a73b23146d45139f3
Ahhasha <elenacheoai@gmail.com>
2025-11-23 20:15:59 +0300

    feat: добавил graceful-shutdown и config

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,15 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"pr-reviewer-service-AVITO2025/internal/config"
 	"pr-reviewer-service-AVITO2025/internal/database"
 	myhttp "pr-reviewer-service-AVITO2025/internal/http"
 	"pr-reviewer-service-AVITO2025/internal/http/handlers"
 	"pr-reviewer-service-AVITO2025/internal/service"
 	"pr-reviewer-service-AVITO2025/internal/storage/postgres"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -18,7 +22,8 @@ func main() {
 
 	lgr := slog.New(slog.NewJSONHandler(os.Stderr, nil))
 
-	databaseURL := "postgresql://postgres:pr_service_password@localhost:5432/pr_service?sslmode=disable"
+	cfg := config.Load()
+	databaseURL := cfg.DatabaseURL()
 
 	pool, err := database.NewPool(ctx, databaseURL)
 	if err != nil {
@@ -41,6 +46,33 @@ func main() {
 
 	router := myhttp.NewRouter(teamHandler, userHandler, prHandler)
 
-	lgr.Info("Server start on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		lgr.Info("Server start on :8080")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	<-ctx.Done()
+	log.Println("timeout of 5 seconds.")
+
+	log.Println("Server exiting")
 }
